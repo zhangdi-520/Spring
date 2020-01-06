1d6da3281db38b44758c311168515fa4273ec1f9
zhangdi-520 <1137453824@qq.com>
2020-01-06 16:28:56 +0800

    Spring项目框架

---
+++ src/main/java/com/spring/soundsystem/SgtPeppers.java
@@ -0,0 +1,15 @@
+package com.spring.soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SgtPeppers implements CompactDisc{
+
+    private String title = "Sgt. Pepper Lonely Hearts Club Band";
+    private String artist = "The Beatles";
+
+    @Override
+    public void play() {
+        System.out.println("Playing " + title + " by "+artist);
+    }
+}
